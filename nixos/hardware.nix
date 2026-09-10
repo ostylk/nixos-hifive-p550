44f3ec20657cc94e252268e5a844666207402ac0
ostylk <ostylk@googlemail.com>
2026-09-10 23:22:32 +0200

    setup kernel, dtb and bootloader for nixos

---
+++ nixos/hardware.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+
+{
+  # Use kernel
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.callPackage ../kernels/linux-6.6.nix { });
+  boot.kernelParams = [
+    "efi=debug"
+    "earlycon=sbi"
+  ];
+
+  # Make sure we use the correct device tree, otherwise the board just won't boot
+  hardware.deviceTree = {
+    enable = true;
+    name = "eswin/eic7700-hifive-premier-p550.dtb";
+  };
+
+  # EFI boot seems to be the preferred way to boot.
+  # (I did not manage to get it to boot without EFI.)
+  boot.loader.systemd-boot = {
+    enable = true;
+    installDeviceTree = true;
+  };
+
+  nixpkgs.hostPlatform = "riscv64-unknown-linux-gnu";
+}
