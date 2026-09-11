c8675a1c3b6244d1868cca32f86e13d6c61e1139
ostylk <ostylk@googlemail.com>
2026-09-11 11:18:21 +0200

    refactor common kernel config

--- kernels/linux-6.12.nix
+++ kernels/linux-6.12.nix
@@ -1,39 +1,18 @@
 {
-  buildLinux,
+  callPackage,
   fetchFromGitHub,
-  lib,
   ...
 }@args:
 
-let
-  # Branch rel/kernel-6.12/hifive-premier-p550
-  src = fetchFromGitHub {
-    owner = "sifiveinc";
-    repo = "riscv-linux";
-    rev = "3340c5ae8c9d093fdf23ed8c68437846cac893b6";
-    hash = "sha256-1bkLMnsd5XoYuWZANBBF0et9LuvZMe5QexMfmpRJQqQ=";
-  };
-
-  makefile = "${src}/Makefile";
-  version = toString (builtins.match ".+VERSION = ([0-9]+).+" (builtins.readFile makefile));
-  patchlevel = toString (builtins.match ".+PATCHLEVEL = ([0-9]+).+" (builtins.readFile makefile));
-  sublevel = toString (builtins.match ".+SUBLEVEL = ([0-9]+).+" (builtins.readFile makefile));
-in
-buildLinux (
+callPackage ./linux-base.nix (
   args
   // {
-    inherit src;
-    version = "${version}.${patchlevel}.${sublevel}";
-
-    defconfig = "hifive-premier-p550_defconfig";
-
-    structuredExtraConfig = with lib.kernel; {
-      SND_SOC_ES8328 = lib.mkForce no;
-      SND_SOC_ES8328_I2C = lib.mkForce no;
-      SND_SOC_ES8328_SPI = lib.mkForce no;
-      ESWIN_MIPI_DSI = lib.mkForce no;
-      DRM_IMG_VOLCANIC = lib.mkForce no;
+    # Branch rel/kernel-6.12/hifive-premier-p550
+    src = fetchFromGitHub {
+      owner = "sifiveinc";
+      repo = "riscv-linux";
+      rev = "3340c5ae8c9d093fdf23ed8c68437846cac893b6";
+      hash = "sha256-1bkLMnsd5XoYuWZANBBF0et9LuvZMe5QexMfmpRJQqQ=";
     };
   }
-  // (args.argsOverride or { })
 )
---
+++ kernels/linux-base.nix
@@ -0,0 +1,34 @@
+{
+  buildLinux,
+  lib,
+  src,
+  ...
+}@args:
+
+let
+  makefile = "${src}/Makefile";
+  version = toString (builtins.match ".+VERSION = ([0-9]+).+" (builtins.readFile makefile));
+  patchlevel = toString (builtins.match ".+PATCHLEVEL = ([0-9]+).+" (builtins.readFile makefile));
+  sublevel = toString (builtins.match ".+SUBLEVEL = ([0-9]+).+" (builtins.readFile makefile));
+in
+buildLinux (
+  args
+  // {
+    inherit src;
+    version = "${version}.${patchlevel}.${sublevel}";
+
+    defconfig = "hifive-premier-p550_defconfig";
+
+    structuredExtraConfig =
+      with lib.kernel;
+      {
+        SND_SOC_ES8328 = lib.mkForce no;
+        SND_SOC_ES8328_I2C = lib.mkForce no;
+        SND_SOC_ES8328_SPI = lib.mkForce no;
+        ESWIN_MIPI_DSI = lib.mkForce no;
+        DRM_IMG_VOLCANIC = lib.mkForce no;
+      }
+      // (args.structuredExtraConfig or { });
+  }
+  // (args.argsOverride or { })
+)
