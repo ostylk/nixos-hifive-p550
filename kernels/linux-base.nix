b262062b313b1d836b9113fadb4a10d9c06db08f
ostylk <ostylk@googlemail.com>
2026-09-22 00:00:42 +0200

    kernel config with NUMA is broken and ethernet needs to be compiled in

--- kernels/linux-base.nix
+++ kernels/linux-base.nix
@@ -32,6 +32,16 @@ buildLinux (
         SND_SOC_ES8328_SPI = lib.mkForce no;
         ESWIN_MIPI_DSI = lib.mkForce no;
         DRM_IMG_VOLCANIC = lib.mkForce no;
+
+        # The eic7700 fdt does not assign nids to CPUs
+        # so the linux kernel panics if NUMA is activated (because of poor vendor code)
+        NUMA = lib.mkForce no;
+
+        # ethernet driver seem to only work if compiled-in...
+        STMMAC_ETH = lib.mkForce yes;
+        STMMAC_PLATFORM = lib.mkForce yes;
+        DWMAC_EIC7700 = lib.mkForce yes;
+        MOTORCOMM_PHY = lib.mkForce yes;
       }
       // (args.structuredExtraConfig or { });
   }
